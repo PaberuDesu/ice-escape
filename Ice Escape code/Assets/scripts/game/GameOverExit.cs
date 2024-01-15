c95ee4651f66acea5d5acac8ef4d7976ff823551
4218 Николаев П.С <pashan1ckolaeff@yandex.ru>
2024-01-15 20:46:07 +0300

    preapocaliptic version
    partly working version before full code change to return to in case of fail

--- Ice Escape code/Assets/scripts/game/GameOverExit.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class GameOverExit : MonoBehaviour
-{
-    public void Restart(){Camera.main.GetComponent<RestartTheGame>().Restart();}
-
-    public void MainMenu(){Camera.main.GetComponent<BackToMenu>().ToMainMenu();}
-}
