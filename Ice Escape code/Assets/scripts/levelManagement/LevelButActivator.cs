0cd808c27a175f2c32621a7d9077b8bcb9501d0e
4218 Николаев П.С <pashan1ckolaeff@yandex.ru>
2024-01-15 15:48:13 +0300

    Level manager redesign
    Adaptive interface and a bit of redesign of the level manager

--- Ice Escape code/Assets/scripts/levelManagement/LevelButActivator.cs
+++ Ice Escape code/Assets/scripts/levelManagement/LevelButActivator.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 
 public class LevelButActivator : MonoBehaviour
 {
-    [SerializeField] private Transform _levelButsParent;
+    [SerializeField] private Transform[] _levelButs;
     public static LevelBunch LevelResults;
 
     private void Awake(){
         LevelResults = new LevelBunch();
         LevelResults = JsonUtility.FromJson<LevelBunch>(File.ReadAllText(Application.streamingAssetsPath + $"/Levels/levelBunch{0}-Progress.json"));
         for(int i = 0; i < 20; i++){
-            Transform Button = _levelButsParent.GetChild(i);
+            Transform Button = _levelButs[i];
             LevelResult result = LevelResults[i];
             Button.GetComponent<Button>().interactable = i>0 ? LevelResults[i-1].IsCompleted : true;
             if(result.IsCompleted){
